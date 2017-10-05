520a75b4ea77bbba672a8736db5b0732c1b8a5ef
Austin White <Austin White>
2017-10-05 18:52:13 -0400

    following eslint

--- src/server.js
+++ src/server.js
@@ -7,16 +7,16 @@ const socketio = require('socket.io');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const handler = (req, res) => {
-  //check if asking for bundle and send that back
-  //otherwise send back html
-  if(req.url === '/bundle.js') { 
+  // check if asking for bundle and send that back
+  // otherwise send back html
+  if (req.url === '/bundle.js') {
     fs.readFile(`${__dirname}/../hosted/bundle.js`, (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'application/javascript'});
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
       res.end(data);
     });
   } else {
     fs.readFile(`${__dirname}/../hosted/index.html`, (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'text/html'});
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
   }
@@ -27,52 +27,51 @@ const app = http.createServer(handler).listen(port);
 console.log(`Listening on 127.0.0.1: ${port}`);
 
 const io = socketio(app);
-let users = {};
-let pos = 400
+const users = {};
+let pos = 400;
 const onJoined = (sock) => {
   const socket = sock;
   socket.join('room');
-  users[socket.id] = Math.ceil(Math.random() * 2)-1
+  users[socket.id] = Math.ceil(Math.random() * 2) - 1;
   // set background color for each player
-  if(users[socket.id] == 0)
-  {
-    let data = {
-      x:0,
-      y:0,
-      width:350,
-      height:300,
-      color:"#FF0000",
-      pos: pos
-    }
+  if (users[socket.id] === 0) {
+    const data = {
+      x: 0,
+      y: 0,
+      width: 350,
+      height: 300,
+      color: '#FF0000',
+      pos,
+    };
     socket.emit('sideChosen', data);
-  }else{
-    let data = {
-      x:400,
-      y:0,
-      width:400,
-      height:300,
-      color:"#0000FF",
-      pos: pos
-    }
+  } else {
+    const data = {
+      x: 400,
+      y: 0,
+      width: 400,
+      height: 300,
+      color: '#0000FF',
+      pos,
+    };
     socket.emit('sideChosen', data);
   }
-  socket.on('move', (data) => {
+  socket.on('move', () => {
     console.log(socket.id);
-   if(users[socket.id] == 0){
-     //move left
-      pos -=1;
-      let data = {
-        pos: pos
-      }
+    if (users[socket.id] === 0) {
+      // move left
+      pos -= 1;
+      const data = {
+        pos,
+      };
       io.sockets.in('room').emit('update', data);
-   }else{
-     //move right
-     pos +=1;
-     let data = {
-      pos: pos
+    } else {
+      // move right
+      pos += 1;
+      const data = {
+        pos,
+      };
+      io.sockets.in('room').emit('update', data);
     }
-     io.sockets.in('room').emit('update', data);
-   }
   });
 };
 
